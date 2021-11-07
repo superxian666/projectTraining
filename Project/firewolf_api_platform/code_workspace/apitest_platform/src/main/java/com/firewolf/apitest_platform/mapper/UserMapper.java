b10260b6ab86d6898fdaf0d20d88a25ceb603ee8
superxian666 <1259346558@qq.com>
2021-11-07 14:41:38 +0800

    mapper/UserMapper.java

--- Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/mapper/UserMapper.java
+++ Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/mapper/UserMapper.java
@@ -15,11 +15,9 @@ public interface UserMapper {
     @Select("select * from user")
     public List<User> findAll();
 
+    @Select("select password from user where username=#{username}")
+    public String login(String username);
+
     @Insert("insert into user values (#{user_id},#{username},#{password})")
     public int insertUser(User user);
-
-    @Select("select user_id from user where username = #{username}")
-    public int findUser(User user);
-
-
 }
