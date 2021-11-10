8092e5ceada1606f8413f87e4f1823aa85b8bb02
superxian666 <1259346558@qq.com>
2021-11-10 19:11:35 +0800

    change login sign

--- Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/UserSgin.java
+++ Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/UserSgin.java
@@ -5,77 +5,46 @@ import com.firewolf.apitest_platform.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-@CrossOrigin
+/**
+ * @author superxian
+ * @date 2021/11/10 10:18
+ */
+
 @RestController
 public class UserSgin {
 
-    /**
-     * @Autowired  引用类型对象使用的注解，加上这个注解后不用实例化。
-     * */
-
     @Autowired
     private UserMapper userMapper;
-/**
- * http://localhost:8080/hello
- * */
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello SpringBoot!!!";
-    }
-
     /**
-     * http://localhost:8080/sginUser?username=xiaozhu&password=123456789
-     * */
+     * http://localhost:8080/register
+     * @return*/
     @CrossOrigin
-    @RequestMapping("/sginUser")
-    public void sgin(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("=========接收了Ajax的请求=========");
-        //接收请求的参数
-        String strName = request.getParameter("username");
-        String strW = request.getParameter("password");
-
-        System.out.println(strName);
-
-        String info="";
-
-        User user = new User();
-        user.setUsername(strName);
-        user.setPassword(strW);
-        int o = userMapper.insertUser(user);
-        System.out.println(o);
-        //使用jsp
-
-        //把数据保存到 request 作用域
-        info = strName +"注册成功";
-        //输出数据
-        response.setContentType("text/html;charset=utf-8");
-        PrintWriter out = response.getWriter();
-
-        out.println(info);
-        out.flush();
-        out.close();
-
-    }
-    @CrossOrigin
-    @RequestMapping(value="/register",method= RequestMethod.POST)
-    @ResponseBody
+    @PostMapping(value="/register",consumes = "application/json;charset=UTF-8")
     public String register(@RequestBody User resister_user) {
-        User user0 = new User();
-        user0.setUsername(resister_user.getUsername());
-        user0.setPassword(resister_user.getPassword());
-        if(userMapper.findUser(resister_user)!=null){
-            return "用户名重复注册失败！";
 
+        String retusername=resister_user.getUsername();
+        String retpassword=resister_user.getPassword();
+        String renpassword=resister_user.getRpassword();
+
+        User user0 = new User();
+        user0.setUsername(retusername);
+        user0.setPassword(retpassword);
+
+        if(userMapper.findUser(resister_user)!=null){
+            System.out.println("用户"+retusername+"已存在！");
+            return "用户"+retusername+"已存在！";
+        }else if(retusername.length()>16 || retusername.length()<6 ){
+            System.out.println("用户名长度不符合要求！");
+            return "用户名长度不符合要求！";
+        }else if(retpassword.length()>16 || retpassword.length()<6){
+            System.out.println("密码长度不符合要求！");
+            return"密码长度不符合要求!";
+        }else if(retpassword.equals(renpassword)==false){
+            return "新密码与确认新密码不一致";
         }else{
             userMapper.insertUser(user0);
-            return "注册成功！";
+            System.out.println(retusername+"注册成功！");
+            return retusername+"注册成功！";
         }
-
     }
-
 }
