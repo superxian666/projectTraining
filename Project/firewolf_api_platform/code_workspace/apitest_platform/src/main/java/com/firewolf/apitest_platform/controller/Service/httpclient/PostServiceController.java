61a066040573dbdbf7c6645cd9892ce933c3a95e
sunnygaoxin <1213977085@qq.com>
2021-11-21 10:11:48 +0800

    update new packeges

--- Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/Service/httpclient/PostServiceController.java
+++ Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/Service/httpclient/PostServiceController.java
@@ -1,66 +1,55 @@
-package com.firewolf.apitest_platform.controller.Service;
+package com.firewolf.apitest_platform.controller.Service.httpclient;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.firewolf.apitest_platform.domain.Case;
 import com.firewolf.apitest_platform.mapper.GetServiceMapper;
 import com.firewolf.apitest_platform.service.HttpClientUtils;
 import com.firewolf.apitest_platform.vo.CommonResult;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Map;
 
-/**
- * 高昕 2021年11月14号
- * GetServiceController ：这个类用于实现get方式的接口测试
- * 1.接收前端发送的参数
- * 2.将这个数据插入数据库，并且有唯一的标识 id collection_id
- *
- * 这个有的业务由其他的控制器来搞，这里的开发遵循最小逻辑单元。
- *
- * 问题： 递增的问题
- * */
+@Controller
+public class PostServiceController {
 
-
-
-
-
-@RestController
-public class GetServiceController {
 
     @Autowired
     private GetServiceMapper dao;
     /**
-     * post方式接收数据param为url,用户选择的请求方式 这里一定是get
-     * 接收到的请求体 比如表单中的method = get,url=****
+     *  执行post的接口测试方法
      *
-     * http://localhost:8080/doService/getServe.do
-     *
-     *
+     *  http://localhost:8080/doService/postService.do
      * */
-    @CrossOrigin
-    @PostMapping(value="/doService/getServe.do")
+
+
     @ResponseBody
-    public CommonResult getServe(
-            HttpServletRequest request,
-            @RequestParam(value = "cid",required = false)Integer cid,
+    @RequestMapping(value = "/doService/postService.do",method = RequestMethod.POST)
+    public CommonResult doPost(
             @RequestParam(value = "url",required = true)String url,
+            @RequestParam(value = "body",required = false)String body,
+            @RequestParam(value = "header",required = false)Map<String,String> headers,
+            @RequestParam(value = "cid",required = false)Integer cid,
             @RequestParam(value = "name",required = false)String name,
             @RequestParam(value = "method",required = false)String method,
             @RequestParam(value = "type",required = false)String type
-    ) throws IOException {
+            ) throws IOException {
         CommonResult cr = new CommonResult();
-
-
-
+        String res =null;
         /**
-         * 获取后把他插入到表中，这里应该怎么设计呢？
+         * 将string类型的body转为json类型
          * */
-        String res = HttpClientUtils.getOnly(url);
+        JSONObject json = JSON.parseObject(body);
 
-        if(res!=null){
+
+        if(url!=null&& body!=null){
+            res = HttpClientUtils.doPost(url,json);
+        }
+
+        if(res!= null){
             cr.setCode(0);//代表成功
             cr.setMsg("获取接口返回值成功");
             cr.setData(res);
@@ -70,7 +59,9 @@ public class GetServiceController {
             cr.setData("");
         }
 
-//        如果查询为空 ，那么就把 case插入表
+        /**
+         * 不管是否查询成功还是失败都会插入到数据库，为客户保存数据
+         * */
         Case cas = new Case(cid,  name,  url,  method,  type);
         Case intable = dao.selectCaseByUrl(cas);
         if(intable==null){
