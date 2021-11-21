72b9bb2a00df2ca80fe4c127d987a44032763814
sunnygaoxin <1213977085@qq.com>
2021-11-21 09:43:19 +0800

    update

--- Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/Service/PostServiceController.java
+++ Project/firewolf_api_platform/code_workspace/apitest_platform/src/main/java/com/firewolf/apitest_platform/controller/Service/PostServiceController.java
@@ -1,9 +1,12 @@
-package com.firewolf.apitest_platform.controller;
+package com.firewolf.apitest_platform.controller.Service;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.firewolf.apitest_platform.domain.Case;
+import com.firewolf.apitest_platform.mapper.GetServiceMapper;
 import com.firewolf.apitest_platform.service.HttpClientUtils;
 import com.firewolf.apitest_platform.vo.CommonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,17 +15,27 @@ import java.util.Map;
 
 @Controller
 public class PostServiceController {
+
+
+    @Autowired
+    private GetServiceMapper dao;
     /**
      *  执行post的接口测试方法
      *
      *  http://localhost:8080/doService/postService.do
      * */
+
+
     @ResponseBody
     @RequestMapping(value = "/doService/postService.do",method = RequestMethod.POST)
     public CommonResult doPost(
             @RequestParam(value = "url",required = true)String url,
             @RequestParam(value = "body",required = false)String body,
-            @RequestParam(value = "header",required = false)Map<String,String> headers
+            @RequestParam(value = "header",required = false)Map<String,String> headers,
+            @RequestParam(value = "cid",required = false)Integer cid,
+            @RequestParam(value = "name",required = false)String name,
+            @RequestParam(value = "method",required = false)String method,
+            @RequestParam(value = "type",required = false)String type
             ) throws IOException {
         CommonResult cr = new CommonResult();
         String res =null;
@@ -44,6 +57,19 @@ public class PostServiceController {
             cr.setCode(1);
             cr.setMsg("获取接口返回值失败");
             cr.setData("");
+        }
+
+        /**
+         * 不管是否查询成功还是失败都会插入到数据库，为客户保存数据
+         * */
+        Case cas = new Case(cid,  name,  url,  method,  type);
+        Case intable = dao.selectCaseByUrl(cas);
+        if(intable==null){
+            /**
+             *  这里需要重新实例化一个对象，不实例化会报错。
+             * */
+            Case cas2 = new Case(cid,  name,  url,  method,  type);
+            dao.insertCase(cas2);
         }
 
         return cr;
